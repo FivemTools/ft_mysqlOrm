249c40458e89fa8d0e75a63c1dc6836b25f3e031
Romain Lanz <romain.lanz@slynova.ch>
2017-08-04 22:11:44 +0200

    chore(Model): document methods

--- src/Model.lua
+++ src/Model.lua
@@ -1,34 +1,72 @@
-local Config = {}
 Model = {}
+local Config = {}
 
+-- -
+-- Configure the model with Table's settings.
+--
+-- @param  {Table}  config
+-- @return {void}
+--
 Model.useConfig = function (config)
   Config.tableName = config.tableName
   Config.primaryKey = config.primaryKey or 'id'
   Config.updateKey = config.updateKey or 'id'
 end
 
+-- -
+-- Create a new Model instance.
+--
+-- @param  {Table}  attributes
+-- @return {Model}
+--
 Model.new = function (attributes)
   local instance = {}
 
   instance.isDirty = false
   instance.attributes = attributes
 
+  -- -
+  -- Set/Change the value of an attribute.
+  --
+  -- @param  {String}          attribute
+  -- @param  {String|Integer}  value
+  -- @return {this}
+  --
   instance.set = function (attribute, value)
     instance.isDirty = true
     instance.attributes[attribute] = value
+
+    return instance
   end
 
+  -- -
+  -- Get the value of an attribute.
+  --
+  -- @param  {String}  attribute
+  -- @return {String|Integer}
+  --
   instance.get = function (attribute)
     return instance.attributes[attribute]
   end
 
+  -- -
+  -- Save the current instance into the database.
+  --
+  -- @return {void}
+  --
   instance.save = function ()
-
+    -- TODO : Generate INSERT/UPDATE SQL
   end
 
   return instance
 end
 
+-- -
+-- Search into the database with the primary key given.
+--
+-- @param  {Integer}  primaryKey
+-- @return {Model}
+--
 Model.find = function (primaryKey)
   return Model.new(
     MySQL.Sync.fetchAll('SELECT * FROM ' .. Config.tableName .. ' WHERE ' .. Config.primaryKey .. ' = @primaryKey',
@@ -36,15 +74,16 @@ Model.find = function (primaryKey)
   )
 end
 
+-- -
+-- Search into the database with the field and value given.
+--
+-- @param  {String}  field
+-- @param  {String}  value
+-- @return {Model}
+--
 Model.findBy = function (field, value)
   return Model.new(
     MySQL.Sync.fetchAll('SELECT * FROM ' .. Config.tableName .. ' WHERE ' .. field .. ' = @value',
     { ['@value'] = value })[1]
   )
-end
-
-Model.create = function (attributes)
-end
-
-Model.update = function (value, fields)
 end
