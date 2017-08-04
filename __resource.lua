8a4a5c9a772d242645339d4f949ffe4bff7614a4
Romain Lanz <romain.lanz@slynova.ch>
2017-08-04 22:00:58 +0200

    chore: change structure of package

--- __resource.lua
+++ __resource.lua
@@ -2,10 +2,10 @@ resource_manifest_version '77731fab-63ca-442c-a67b-abc70f28dfa5'
 
 server_scripts {
   '@mysql-async/lib/MySQL.lua',
-  'Table.lua',
-  'Model.lua',
-  'exports.lua',
-  'events.lua',
+  'src/Table.lua',
+  'src/Model.lua',
+  'src/exports.lua',
+  'src/events.lua',
 }
 
 server_exports {
