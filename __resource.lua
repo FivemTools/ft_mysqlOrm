a6dc64b3d46f4961b83d9d1e334321f8b980d7aa
Romain Lanz <romain.lanz@slynova.ch>
2017-08-04 22:15:07 +0200

    chore: move global files to root

--- __resource.lua
+++ __resource.lua
@@ -1,11 +1,19 @@
+-- -
+-- @Project  FiveM Tools
+-- @License  GNU General Public License v3.0
+-- @Version  2017.08.04
+--
+
 resource_manifest_version '77731fab-63ca-442c-a67b-abc70f28dfa5'
 
 server_scripts {
   '@mysql-async/lib/MySQL.lua',
+
   'src/Table.lua',
   'src/Model.lua',
-  'src/exports.lua',
-  'src/events.lua',
+
+  '_exports.lua',
+  '_events.lua',
 }
 
 server_exports {
