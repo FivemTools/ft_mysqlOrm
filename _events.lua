c490688a63cc187ad4581104b6e1f8f537799c11
Romain Lanz <romain.lanz@slynova.ch>
2017-08-04 22:48:32 +0200

    chore: add header to all files

--- _events.lua
+++ _events.lua
@@ -1,3 +1,9 @@
+-- -
+-- @Project  FiveM Tools
+-- @License  GNU General Public License v3.0
+-- @Version  2017.08.04
+--
+
 RegisterServerEvent('ft_orm:Loaded')
 AddEventHandler('onServerResourceStart', function (resource)
   if resource == "ft_orm" then
